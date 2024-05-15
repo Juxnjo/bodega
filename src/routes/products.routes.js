36f555890a5674b16fea2263b7946ab318247422
“Juan <ramirezjuanj0117@gmail.com>
2024-05-15 13:07:40 -0500

    CRUD OK

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,6 +3,7 @@ import { pool } from "../db.js";
 
 const router = Router()
 
+//GET
 router.get('/products', async (req, res) => {
     const {rows} = await pool.query('SELECT * FROM products')
     res.json(rows)
@@ -16,32 +17,40 @@ router.get('/products/:id', async (req, res)=> {
         return res.status(404).json({message: 'Product not found'})
     }
 
-    res.json(rows)
+    res.json(rows[0])
 })
 
+//POST
 router.post('/products', async (req, res) => {
     const data = req.body
-    console.log(data)
-    const result = await pool.query('INSERT INTO products (code, name) VALUES ($1, $2)', [data.code, data.name])
-    console.log(result)
-    res.send('creando productos')
+    
+    const {rows} = await pool.query('INSERT INTO products (code, name) VALUES ($1, $2) RETURNING *', [data.code, data.name])
+    console.log(rows)
+    
+    return res.json(rows[0])
 })
 
+//DELETE
 router.delete('/products/:id', async (req, res) => {
     const {id} =  req.params
-    const {rows, rowCount} = await pool.query('DELETE FROM products WHERE id = $1 RETURNING *', [id])
+    const { rowCount} = await pool.query('DELETE FROM products WHERE id = $1 RETURNING *', [id])
     console.log(rows)
 
     if (rowCount === 0){
         return res.status(404).json({message: "Product not found"})
     }
     
-    return res.json({message: 'Product deleted'})
+    return res.sendStatus(204)
 })
 
-router.put('/products/:id', (req, res) => {
+//PUT
+router.put('/products/:id', async (req, res) => {
     const {id} =  req.params
-    res.send('actualizando productos' + productId)
+    const data = req.body
+
+    const {rows} = await pool.query('UPDATE products SET code = $1, name = $2, updated_at = $3 WHERE id = $4 RETURNING *', [data.code, data.name, data.updated_at, id] )
+
+    return res.json(rows[0])
 })
 
 export default router 
