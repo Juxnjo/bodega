35f2c8204502b689f4248113aa3029c49ba33c1a
“Juan <ramirezjuanj0117@gmail.com>
2024-06-06 19:25:16 -0500

    frontend setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from "cors";
 import { PORT } from './config.js'
 
 import productRoutes from './routes/products.routes.js'
@@ -8,6 +9,7 @@ import inventoryRoutes from "./routes/inventory.routes.js";
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 
